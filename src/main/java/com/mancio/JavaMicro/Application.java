2722b93dd5100417370f717f947d0b5d20628d95
Andrea Mancini <mancioman2@gmail.com>
2020-06-19 15:13:13 +0200

    fix okta authentication

--- src/main/java/com/mancio/JavaMicro/Application.java
+++ src/main/java/com/mancio/JavaMicro/Application.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableCaching      // to enable spring cache
