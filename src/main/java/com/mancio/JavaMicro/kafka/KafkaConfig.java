9d6a416707d39e0312d1aef6bd0a39ec2b5af6d1
Andrea Mancini <mancioman2@gmail.com>
2020-07-17 15:39:23 +0200

    change kafka port

--- src/main/java/com/mancio/JavaMicro/kafka/KafkaConfig.java
+++ src/main/java/com/mancio/JavaMicro/kafka/KafkaConfig.java
@@ -19,7 +19,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, EmployeesRating> producerFactory(){
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9093");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory(config);
